334fa69dabca93a426b8122ece5a93b9673605e9
Quentin Barnard <qbarnard@gmail.com>
2023-01-08 18:18:18 +0200

    updated the ready

---
+++ example/src/App.js
@@ -0,0 +1,11 @@
+import { CircularSlider } from 'react-web-circular-slider';
+
+export const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<CircularSlider />
+			</header>
+		</div>
+	);
+};
--- example/src/index.js
+++ example/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
-import { CircularSlider } from 'react-web-circular-slider';
+import { App } from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<div>
-		<CircularSlider />
-	</div>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
